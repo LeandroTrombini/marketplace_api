93017d8030231f8fdd7d84e8fc7bfe525a41a3b2
trombini <leandro.ae@gmail.com>
2023-12-03 11:28:48 -0300

    Adds more tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,20 @@ RSpec.describe User, type: :model do
 
   subject { user }
 
-  it { should respond_to(:email) }
-  it { should respond_to(:password) }
-  it { should respond_to(:password_confirmation) }
+  describe 'When respond to email and password' do
+    it { should respond_to(:email) }
+    it { should respond_to(:password) }
+    it { should respond_to(:password_confirmation) }
+  end
 
-  it { should be_valid }
+  describe 'When email is present' do
+    it { should be_valid }
+  end
+
+  describe 'When email is not present' do
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_confirmation_of(:password) }
+    it { should validate_presence_of(:email) }
+    it { should allow_values('email@email.com').for(:email) }
+  end
 end
