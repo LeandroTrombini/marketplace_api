e8c25aa0b20fa73ad1eb3cd242a32f746b794989
trombini <leandro.ae@gmail.com>
2023-12-08 21:55:57 -0300

    Adds shows action to the users controllers

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :users, only: [:show]
     end
   end
 end
