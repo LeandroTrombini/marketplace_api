f5cc47a349a12a7ff6b8b15a2c66a5e4345e94fc
trombini <leandro.ae@gmail.com>
2024-01-09 11:01:57 -0300

    Adds user authentication token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe User, type: :model do
     it { should respond_to(:email) }
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }
+    it { should respond_to(:token) }
   end
 
   describe 'When email is present' do
@@ -22,5 +23,24 @@ RSpec.describe User, type: :model do
     it { should validate_confirmation_of(:password) }
     it { should validate_presence_of(:email) }
     it { should allow_values('email@email.com').for(:email) }
+  end
+
+  describe 'When token is valid' do
+    it { should validate_uniqueness_of(:token) }
+  end
+
+  describe '#generate_authentication_token!' do
+    it 'generates a unique token' do
+      token = 'r0lf025azsz5s51sa'   
+      allow(Devise).to receive(:friendly_token).and_return(token)
+      user.generate_authentication_token!
+      expect(user.token).to eq token
+    end
+
+    it 'generates another token when one has been taken' do
+      exists_user = FactoryBot.create(:user, token: 'r0lf025azsz5s51sa')      
+      user.generate_authentication_token!
+      expect(user.token).not_to eq exists_user.token
+    end
   end
 end
