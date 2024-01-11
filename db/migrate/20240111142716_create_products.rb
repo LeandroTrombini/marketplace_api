bd3eadbbb2b07c35bc1676c1255016b191983f9e
trombini <leandro.ae@gmail.com>
2024-01-11 11:36:05 -0300

    Adds product model along with some validations and associations

---
+++ db/migrate/20240111142716_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.decimal :price
+      t.text :description
+      t.boolean :published
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
